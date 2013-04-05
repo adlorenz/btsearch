faf1bb679aec284b5e3781ef9ffbf74cd2b3feb8
Dawid Lorenz <dawid@lorenz.co>
2013-04-06 00:36:20 +0200

    Various tweaks throughout the database browser

--- src/btsearch/bts/views.py
+++ src/btsearch/bts/views.py
@@ -5,22 +5,28 @@ from btsearch.bts.models import BaseStation
 
 
 class SearchRedirectView(RedirectView):
+
     def get_redirect_url(self):
-        return reverse('bts-query', kwargs={'query': self.request.GET['search']})
+        search_query = self.request.GET.get('search', None)
+        if search_query:
+            return reverse('bts-query', kwargs={'query': search_query})
+        else:
+            return reverse('bts-listing')
 
 
 class BtsListingView(ListView):
     template_name = 'bts/listing.html'
     model = BaseStation
-    queryset = BaseStation.objects.all()
+    queryset = BaseStation.objects.order_by('-date_updated')
     context_object_name = 'basestations'
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        context = super(BtsListingView, self).get_context_data(**kwargs)
+        ctx = super(BtsListingView, self).get_context_data(**kwargs)
+        ctx['num_rows'] = self.get_queryset().count()
         if 'query' in self.kwargs:
-            context['q'] = self.kwargs['query']
-        return context
+            ctx['q'] = self.kwargs['query']
+        return ctx
 
     def get_queryset(self):
         qs = super(BtsListingView, self).get_queryset()
