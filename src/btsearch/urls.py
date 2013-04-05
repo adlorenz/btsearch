aa41f9864babd6f46b7d0ce9adef34f5f9f2baf9
Dawid Lorenz <dawid@lorenz.co>
2013-04-05 16:38:38 +0200

    Disabled API

--- src/btsearch/urls.py
+++ src/btsearch/urls.py
@@ -1,26 +1,31 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
-from btsearch.bts.api import BaseStationResource, LocationResource, RegionResource, NetworkResource, CellResource
+#from btsearch.bts.api import BaseStationResource, LocationResource, RegionResource, NetworkResource, CellResource
 from btsearch.map.api import LocationResource as MapLocationResource, UkeLocationResource
 from btsearch.map.views import BaseStationExtendedInfoView, ControlPanelView, IndexView, StatusPanelView, UkeLocationExtendedInfoView
 
+# API won't be used for time being
+'''
 api_v1 = Api(api_name='v1')
 api_v1.register(LocationResource())
 api_v1.register(BaseStationResource())
 api_v1.register(RegionResource())
 api_v1.register(NetworkResource())
 api_v1.register(CellResource())
+'''
 
+# API to serve map-related requests
 api_map = Api(api_name='map')
 api_map.register(MapLocationResource())
 api_map.register(UkeLocationResource())
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(api_v1.urls)),
+    #url(r'^api/', include(api_v1.urls)),
     url(r'^api/', include(api_map.urls)),
     url(r'^db', include('btsearch.bts.urls')),
     url(r'^get_control_panel/$', ControlPanelView.as_view(), name='control-panel-view'),
