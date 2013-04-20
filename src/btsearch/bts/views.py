0f99c2924386f1beded6d928904d994a1e50d835
Dawid Lorenz <dawid@lorenz.co>
2013-04-21 00:16:42 +0200

    First stab at database browser filtering

--- src/btsearch/bts/views.py
+++ src/btsearch/bts/views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.views.generic import ListView, RedirectView
-from btsearch.bts.models import BaseStation
+from btsearch.bts.models import BaseStation, Network, Cell
 
 
 class SearchRedirectView(RedirectView):
@@ -23,9 +23,13 @@ class BtsListingView(ListView):
 
     def get_context_data(self, **kwargs):
         ctx = super(BtsListingView, self).get_context_data(**kwargs)
-        ctx['num_rows'] = self.get_queryset().count()
         if 'query' in self.kwargs:
             ctx['q'] = self.kwargs['query']
+
+        ctx['networks'] = Network.objects.all()
+        ctx['standards'] = Cell.STANDARDS
+        ctx['bands'] = Cell.BANDS
+        # ctx['num_rows'] = self.get_queryset().count()
         return ctx
 
     def get_queryset(self):
@@ -33,6 +37,19 @@ class BtsListingView(ListView):
         if 'query' in self.kwargs:
             query = self.kwargs['query']
             qs = qs.filter(Q(location__town__icontains=query) |
-                           Q(location__address__icontains=query))
+                           Q(location__address__icontains=query) |
+                           Q(station_id=query))
+
+        filters = dict(self.request.GET)
+        if 'network' in filters and int(filters['network'][0]) > -1:
+            qs = qs.filter(network__code=filters['network'][0])
+
+        if 'standard' in filters and 'band' in filters:
+            qs = qs.filter(cell__standard__in=list(filters['standard']),
+                           cell__band__in=list(filters['band']))
+        elif 'standard' in filters:
+            qs = qs.filter(cell__standard__in=list(filters['standard']))
+        elif 'band' in filters:
+            qs = qs.filter(cell__band__in=list(filters['band']))
 
         return qs
