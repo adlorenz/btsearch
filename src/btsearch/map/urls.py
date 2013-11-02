c0e3a9823237c841c93ca0ece034f6fca2b805ca
Dawid Lorenz <dawid@lorenz.co>
2013-11-03 00:55:01 +0100

    Add views for UKE locations

--- src/btsearch/map/urls.py
+++ src/btsearch/map/urls.py
@@ -3,15 +3,26 @@ from . import views
 
 urlpatterns = patterns(
     '',
+    url(r'^ukelocations/(?P<pk>\d+)/$',
+        views.UkeLocationDetailView.as_view(),
+        name='locations'),
+
+    url(r'^ukelocations/$',
+        views.UkeLocationsView.as_view(),
+        name='locations'),
+
     url(r'^locations/(?P<pk>\d+)/$',
         views.LocationDetailView.as_view(),
         name='locations'),
+
     url(r'^locations/$',
         views.LocationsView.as_view(),
         name='locations'),
+
     url(r'^ui/control_panel/$',
         views.ControlPanelView.as_view(),
         name='control-panel-view'),
+
     url(r'^ui/status_panel/$',
         views.StatusPanelView.as_view(),
         name='status-panel-view'),
--- src/btsearch/map/views.py
+++ src/btsearch/map/views.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
 from django.http import Http404
 from django.views import generic
 
@@ -8,12 +6,6 @@ from braces.views import JSONResponseMixin
 from ..bts import models as bts_models
 from ..uke import models as uke_models
 from . import utils
-
-
-from django.views.generic import DetailView, TemplateView
-from django.template import Context
-from django.template.loader import get_template
-from settings import STATIC_URL
 
 
 class IndexView(generic.TemplateView):
@@ -31,9 +23,9 @@ class LocationsView(JSONResponseMixin, generic.ListView):
         })
 
     def get_queryset(self):
-        qs = super(LocationsView, self).get_queryset()
+        # qs = super(LocationsView, self).get_queryset()
         qs_filters = self._get_queryset_filters()
-        return qs.filter(**qs_filters)
+        return self.queryset.filter(**qs_filters)
 
     def _get_queryset_filters(self):
         """
@@ -149,6 +141,47 @@ class LocationDetailView(JSONResponseMixin, generic.DetailView):
         return queryset_filters
 
 
+class UkeLocationsView(LocationsView):
+    model = uke_models.UkeLocation
+    queryset = uke_models.UkeLocation.objects.distinct()
+
+    def get_queryset(self):
+        # qs = super(LocationsView, self).get_queryset()
+        qs_filters = self._get_queryset_filters()
+        return self.queryset.filter(**qs_filters)
+
+
+class UkeLocationDetailView(LocationDetailView):
+    model = uke_models.UkeLocation
+    template_name = 'map/uke_location_info.html'
+
+    def _get_location_objects(self, location):
+        """
+        Returns objects associated to the location,
+        ie. base stations or UKE permissions
+        """
+        permissions_by_network = {}
+        networks = []
+        filters = self._get_queryset_filters()
+        for permission in location.get_permissions(**filters):
+            network = permission.network
+            if network not in networks:
+                networks.append(network)
+
+            if network.code not in permissions_by_network:
+                permissions_by_network[network.code] = []
+            permissions_by_network[network.code].append(permission)
+
+        location_objects = []
+        for network in networks:
+            location_objects.append({
+                'network': network,
+                'supported': location.get_supported_standards_and_bands_by_network(network),
+                'permissions': permissions_by_network[network.code],
+            })
+        return location_objects
+
+
 # #####################
 # MAP UI ELEMENTS VIEWS
 # #####################
@@ -167,61 +200,3 @@ class ControlPanelView(generic.TemplateView):
 
 class StatusPanelView(generic.TemplateView):
     template_name = 'map/status_panel.html'
-
-
-# ################
-# DEPRECATED VIEWS
-# ################
-
-
-class LocationView():
-    template_name = 'map/location_info.html'
-    location = None
-    raw_filters = {}
-
-    def __init__(self, location, raw_filters):
-        self.location = location
-        self.raw_filters = raw_filters
-
-    def get_location_items(self):
-        filters = self.get_processed_filters()
-        return self.location.get_base_stations(**filters)
-
-    def get_processed_filters(self):
-        filters = {}
-        if 'standard' in self.raw_filters:
-            filters['standard'] = self.raw_filters['standard'][0].split(',')
-        if 'band' in self.raw_filters:
-            filters['band'] = self.raw_filters['band'][0].split(',')
-        return filters
-
-    def render_location_info(self):
-        template = get_template(self.template_name)
-        context = {'location': self.location,
-                   'items': self.get_location_items(),
-                   'STATIC_URL': STATIC_URL}
-        return template.render(Context(context))
-
-
-class UkeLocationView(LocationView):
-    template_name = 'map/uke_location_info.html'
-
-    def get_location_items(self):
-        permissions_by_network = {}
-        networks = []
-        filters = self.get_processed_filters()
-        for permission in self.location.get_permissions(**filters):
-            network = permission.network
-            if network not in networks:
-                networks.append(network)
-
-            if not permissions_by_network.has_key(network.code):
-                permissions_by_network[network.code] = []
-            permissions_by_network[network.code].append(permission)
-
-        items = []
-        for network in networks:
-            items.append({'network': network,
-                          'supported': self.location.get_supported_standards_and_bands_by_network(network),
-                          'permissions': permissions_by_network[network.code]})
-        return items
