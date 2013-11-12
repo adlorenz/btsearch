151eafa0f47c9015ca6c17e61b638e63744ef42c
Dawid Lorenz <dawid@lorenz.co>
2013-11-13 00:41:03 +0100

    Let test config into the repo for time being

---
+++ src/conf/test.py
@@ -0,0 +1,15 @@
+from conf.default import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'btsearch_app',
+        'PASSWORD': '029ef8e6-1660-4a25-94b4-8d10f7d08adb',
+        'HOST': 'localhost',
+        'NAME': 'Btsearch2',
+    }
+}
+
+EMAIL_SUBJECT_PREFIX = '[Btsearch][Test] '
+
+LOGGING = create_logging_dict(location('../../logs/test'))
