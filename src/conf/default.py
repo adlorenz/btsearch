d6c3701cdc5d445fbc1c606f5dc1a663e98f5f40
Dawid Lorenz <dawid@lorenz.co>
2013-11-13 00:43:42 +0100

    Remove compressor reference in default config

--- src/conf/default.py
+++ src/conf/default.py
@@ -70,7 +70,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 )
 
 # Make this unique, and don't share it with anybody.
