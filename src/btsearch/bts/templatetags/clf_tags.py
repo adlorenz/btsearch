392f392c44676ae1562910e348353804e5881210
Dawid Lorenz <dawid@lorenz.co>
2014-01-05 23:37:18 +0100

    Minor improvement

--- src/btsearch/bts/templatetags/clf_tags.py
+++ src/btsearch/bts/templatetags/clf_tags.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.filter
 def metadata(object):
-    # [{{ cell.base_station.station_id}}:{{ cell.base_station.rnc }}:{% if cell.standard != '?' %}{{ cell.standard|first }}{{ cell.band }}{% endif %}{% if cell.standard == 'UMTS' %}-{{ cell.ua_freq }}{% endif %}]
     cell = object
     metadata = []
     metadata.append(cell.base_station.location.region.code)
@@ -17,4 +16,4 @@ def metadata(object):
         if cell.standard == 'UMTS':
             stdbnd += '-{}'.format(cell.ua_freq)
         metadata.append(stdbnd)
-    return ':'.join(map(lambda x: str(x), metadata))
+    return ':'.join([str(item) for item in metadata])
