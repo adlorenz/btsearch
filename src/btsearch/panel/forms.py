a4adfc51d6a042824bec140f526f50e5e2e1c266
Dawid Lorenz <dawid@lorenz.co>
2014-01-28 00:22:32 +0100

    Add location selector map

--- src/btsearch/panel/forms.py
+++ src/btsearch/panel/forms.py
@@ -3,26 +3,15 @@ from django import forms
 from django.forms.models import inlineformset_factory
 
 from ..bts import models
-from ..bts import widgets
 
 
 class BaseStationEditForm(forms.ModelForm):
 
-    location_info = forms.CharField(max_length=255, required=False)
-
     class Meta:
         model = models.BaseStation
         widgets = {
-            'location': widgets.LocationSelectorWidget()
+            'location': forms.HiddenInput(),
         }
-
-    def __init__(self, *args, **kwargs):
-        super(BaseStationEditForm, self).__init__(*args, **kwargs)
-        self.fields['location'].widget = forms.HiddenInput()
-        if 'instance' in kwargs and kwargs['instance']:
-            instance = kwargs['instance']
-            self.initial['location_info'] = instance.location
-            self.fields['location_info'].widget.attrs['readonly'] = True
 
 BaseStationCellsFormSet = inlineformset_factory(
     models.BaseStation,
