058b9985b221d59ec7d23781f5cd6ac8b7e0bca2
Zachary Milonas <zachary@milonas.pl>
2022-01-16 02:29:06 +0100

    change path for entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
+set -eu
 
-python manage.py syncdb
-python manage.py migrate
-python manage.py "$@"
+until mysql -h "${MYSQL_HOST}" -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} -e 'select 1'; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+python src/manage.py syncdb --noinput
+python src/manage.py migrate --noinput
+python src/manage.py runserver 0.0.0.0:8000
